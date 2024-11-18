d351c8ebbf4a6eff84cfb599c7ef0e0d63f96d6d
Pujan Modha <pujanmodha19@gmail.com>
2024-11-18 21:47:22 +0530

    refactor schema

---
+++ src/app/api/generate/new.ts
@@ -0,0 +1,15 @@
+//prepare array of articles from xml
+//generate articles and save article for each array item
+//task table - no of articles, success, errors, status - completed, incomplete(default), ongoing, started time and end time, article info and version info
+//cron job - every 5 mins
+//fetch task with staus incomplete and err count 0
+//follow 1 and 2
+//update task table with status and time
+/*{
+version1:{
+[
+website, langauge
+]
+}
+
+}*/
