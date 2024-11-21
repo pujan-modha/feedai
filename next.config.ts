243d75866a625b1a742d91b7141d1fdf263c9b26
someonetookmycode <anvit.dd@gmail.com>
2024-11-22 01:31:01 +0530

    next config change

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
+const nextConfig: import("next").NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
